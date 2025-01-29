d53126d3810de8bd22b3d31ac39e8dd89d4403c7
Andrus Diaz Aleman <adiaz@somosproper.com>
2025-01-29 03:23:55 -0400

    feat!: remove criteria filter require (default []) and remove criteria order require (default none)

--- filterValue.go
+++ filterValue.go
@@ -2,13 +2,19 @@ package criteria
 
 type FilterValue interface {
 	GetValue() any
+	IsEmpty() bool
 }
+
 type filterValue struct {
 	value any
 }
 
 func (f *filterValue) GetValue() any {
 	return f.value
+}
+
+func (f *filterValue) IsEmpty() bool {
+	return f.value == nil
 }
 
 func NewFilterValue(value any) FilterValue {
